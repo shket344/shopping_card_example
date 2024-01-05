9b2a8de720d4ccc80df984da5deb0e9502a56dc1
shket344 <kalimador@tut.by>
2024-01-05 17:06:31 +0300

    (SCE-2): create products and cart

---
+++ db/migrate/20240105101526_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price, precision: 5, scale: 20
+
+      t.timestamps
+    end
+  end
+end
